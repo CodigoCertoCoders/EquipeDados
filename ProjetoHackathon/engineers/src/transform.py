66189078fa32bee1c6861e97722f73eb833d04ae
Antonio <jhunny.tec@gmail.com>
2024-08-24 21:33:57 -0300

    refatorando o formato de data na transformação de dados

--- ProjetoHackathon/engineers/src/transform.py
+++ ProjetoHackathon/engineers/src/transform.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import warnings
+
+warnings.filterwarnings("ignore", 
+                        message="Could not infer format, so each element will be parsed individually, "
+                        "falling back to `dateutil`")
 
 def formatar_telefone(numero):
     """Formata números de telefone para o formato +XXXXXXXXXXXXXXX.
@@ -59,14 +64,14 @@ def main():
     'Disponibilidade de Horário:': 'disponibilidade_horario',
     'Preferências de Colaboração: (Trabalhar em equipe na área de TI é essencial para o sucesso de projetos. A colaboração permite a combinação de diversas habilidades e experiências, promove a troca de conhecimentos, estimula a criatividade e acelera o processo de aprendizado.)': 'preferencias_colaboracao',
     'Gostaria de Liderar Projetos?': 'liderar_projetos'
-}, inplace=True)
-    
+    }, inplace=True)
     df_planilha_hackaton['timestamp'] = pd.to_datetime(df_planilha_hackaton['timestamp'], errors='coerce')
+    df_planilha_hackaton['timestamp'] = df_planilha_hackaton['timestamp'].dt.strftime('%d/%m/%Y - %H:%M:%S')
     df_planilha_hackaton['nome_completo'] = df_planilha_hackaton['nome_completo'].str.title()
     df_planilha_hackaton['whatsapp_numero'] = df_planilha_hackaton['whatsapp_numero'].apply(formatar_telefone)
     df_planilha_hackaton['frameworks_bibliotecas'] = df_planilha_hackaton['frameworks_bibliotecas'].apply(formatar_frameworks)
-
     return df_planilha_hackaton
     
+
 if __name__ == '__main__':
     main()
