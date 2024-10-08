44cfebab472ee396bebb80cfea97bdb79066644e
Yasmim <y_abrahao@id.uff.br>
2024-10-08 12:15:49 -0300

    refactor: mudando forma de pegar o csv

--- TimesHackathon/engineers/src/extract.py
+++ TimesHackathon/engineers/src/extract.py
@@ -1,37 +1,15 @@
-import os
-import gdown
+import requests 
+import io
 
-def baixar_arquivos_google_drive(url_pasta, diretorio_local):
-    'Baixa todos os arquivos de uma pasta do Google Drive para o diretório local especificado.'
-    os.makedirs(diretorio_local, exist_ok=True)
+def retornar_arquivo():
+    url_arquivo = 'https://drive.google.com/uc?id=1JLpuGMPkhqYBCvC07k53lh0wtSz2UYGH&export=download'
+    response = requests.get(url_arquivo)
+
     try:
-        gdown.download_folder(url_pasta, output=diretorio_local, quiet=False, use_cookies=False)
-    except Exception as e:
-        print(f'Erro ao baixar arquivos: {e}')
-        return False
-    return True
-
-def listar_arquivos_csv(diretorio):
-    '''Lista todos os arquivos CSV no diretório especificado e os retorna como uma lista de caminhos completos.'''
-    try:
-        arquivos_csv = [os.path.join(diretorio, arquivo) for arquivo in os.listdir(diretorio) if arquivo.endswith('.csv')]
-        return arquivos_csv
-    except FileNotFoundError:
-        print(f'O diretório \'{diretorio}\' não foi encontrado.')
-        return []
-    except PermissionError:
-        print(f'Permissão negada ao acessar o diretório \'{diretorio}\'.')
-        return []
-
-def main():
-    url_pasta = 'https://drive.google.com/drive/folders/1TkyHYcJ0U_dWYeoxaBY_di-WgLNlA5mF'
-    diretorio_local = './Data'
+        conteudo = io.StringIO(response.text)
+        return conteudo
+    except:
+        return None
     
-    if baixar_arquivos_google_drive(url_pasta, diretorio_local):
-        arquivos_csv = listar_arquivos_csv(diretorio_local)
-        print('Arquivos CSV encontrados:', arquivos_csv)
-    else:
-        print('Falha no download dos arquivos.')
-
 if __name__ == '__main__':
-    main()
+    retornar_arquivo()
